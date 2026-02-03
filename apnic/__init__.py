d045f5b3115edb41f1f2ec5b1daf680b5da6879c
Yeh <iap@YehdeMacBook-Pro.local>
2026-02-03 15:16:07 +0800

    modification

--- apnic/__init__.py
+++ apnic/__init__.py
@@ -1,5 +1,3 @@
 from .apnicregistryapi import setLogger,ApnicApiClient, APIRequestError
 from .whoisclient import ApnicWhoisClient
 from .apnicregistryapientry import whois_entry_factory
-from .twnicagent import TwnicAgent
-from .twnicdata import TwnicData
