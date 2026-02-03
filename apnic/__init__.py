4823ea9b27153839714fe74f9b52df62dbd5cb4d
Yeh <iap@YehdeMacBook-Pro.local>
2026-02-03 14:39:50 +0800

    add as a folder

---
+++ apnic/__init__.py
@@ -0,0 +1,5 @@
+from .apnicregistryapi import setLogger,ApnicApiClient, APIRequestError
+from .whoisclient import ApnicWhoisClient
+from .apnicregistryapientry import whois_entry_factory
+from .twnicagent import TwnicAgent
+from .twnicdata import TwnicData
